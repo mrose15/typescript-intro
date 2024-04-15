4e507606b736e76df8a76f8ef8a9b630a0336eec
Michele Cheow <ldnallen@gmail.com>
2024-04-15 14:17:38 -0700

    Enable error reporting for expressions and declarations with an implied 'any' type, reviewing ts types, added ts map for debugging

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
-console.log("Hello World");
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+let numbers = [];
+numbers.forEach((n) => n.toExponential);
+let user = [1, "Mosh"];
+user.push(1);
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,43 @@
-console.log("Hello World");
+/* TYPES */
 
-let age: number = 20;
-if (age < 50) age += 10;
-console.log(age);
+/** Any **/
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level; //any
+
+// function render(document){
+//     console.log(document);
+// }
+
+/** Array **/
+// initialize number array need to be explicitly set to array type
+let numbers: number[] = [];
+// useful for code completion
+numbers.forEach((n) => n.toExponential);
+
+/** Tuples **/
+/*
+- fixed length array where each elements has a particular type
+- often used when working with a pair of values, ie: user has id and name pair
+*/
+
+let user: [number, string] = [1, "Mosh"];
+user.push(1); // gap in TS
+
+/** Enums **/
+/*
+- list of related constants
+*/
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascalCase
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+console.log(mySize);
